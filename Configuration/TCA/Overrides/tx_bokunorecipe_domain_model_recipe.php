30112d17083022a8bdb5719c2e5936267c8afc75
Markus Ketterer <markus.ketterer@gugler.at>
2021-11-10 16:03:46 +0100

    [Task] added TCA for Slug field

--- Configuration/TCA/Overrides/tx_bokunorecipe_domain_model_recipe.php
+++ Configuration/TCA/Overrides/tx_bokunorecipe_domain_model_recipe.php
@@ -5,3 +5,18 @@ defined('TYPO3_MODE') || die();
    'bokunorecipe',
    'tx_bokunorecipe_domain_model_recipe'
 );
+
+$GLOBALS['TCA']['tx_bokunorecipe_domain_model_recipe']['columns']['slug']['config']=[
+   'type' => 'slug',
+   'generatorOptions' => [
+       'fields' => ['title', 'nav_title'],
+       'fieldSeparator' => '/',
+       'prefixParentPageSlug' => true,
+       'replacements' => [
+           '/' => '',
+       ],
+   ],
+   'fallbackCharacter' => '-',
+   'eval' => 'uniqueInSite',
+   'default' => ''
+];
