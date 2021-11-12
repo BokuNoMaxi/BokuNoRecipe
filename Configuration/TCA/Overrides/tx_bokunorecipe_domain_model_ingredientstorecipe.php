9d1e1837a6f2f2bb39e9345644b7016684e23ffd
Markus Ketterer <markus.ketterer@gugler.at>
2021-11-12 12:39:56 +0100

    [Task] renamed package to bunch and added pack of

--- Configuration/TCA/Overrides/tx_bokunorecipe_domain_model_ingredientstorecipe.php
+++ Configuration/TCA/Overrides/tx_bokunorecipe_domain_model_ingredientstorecipe.php
@@ -6,10 +6,12 @@ $GLOBALS['TCA']['tx_bokunorecipe_domain_model_ingredientstorecipe']['columns']['
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.teaspoon', 1],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.tablespoon', 2],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.knifepoint', 3],
-        ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.package', 4],
+        ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.bunch', 4],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.need', 5],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.pinch', 6],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.handful', 7],
         ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.optional', 8],
+        ['LLL:EXT:bokunorecipe/Resources/Private/Language/locallang_db.xlf:tx_bokunorecipe_bokunoingredients.alternativemessurement.pack', 9],
+
 ];
 
