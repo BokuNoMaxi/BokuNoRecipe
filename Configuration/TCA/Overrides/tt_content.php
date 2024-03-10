b72c52827f3216df0d90856506bd95fbc3213ffe
Markus Ketterer <markus.ketterer@gugler.at>
2024-03-10 12:25:46 +0100

    add flexform to Bokunorecipe plugin / set detailPid

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,13 +1,22 @@
 <?php
 defined('TYPO3') || die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+$pluginSignature = ExtensionUtility::registerPlugin(
     'Bokunorecipe',
     'Bokunorecipe',
     'bokunorecipe'
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$GLOBALS["TCA"]["tt_content"]["types"]["list"]["subtypes_addlist"][$pluginSignature] = "pi_flexform";
+ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    "FILE:EXT:bokunorecipe/Configuration/FlexForms/Default.xml"
+);
+
+ExtensionUtility::registerPlugin(
     'Bokunorecipe',
     'Bokunocookinghelper',
     'bokunocookinghelper'
